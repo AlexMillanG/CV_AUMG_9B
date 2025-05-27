188a8077614503fd35f388bc98a0f78c957a982a
AlexMillanG <alexmillanguillen@gmail.com>
2025-05-27 15:50:53 -0600

    [FIRST COMMIT] subida inicial del proyecto

---
+++ src/main/java/mx/edu/utez/cv_aumg_9b/CvAumg9BApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cv_aumg_9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvAumg9BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvAumg9BApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cv_aumg_9b/CvAumg9BApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cv_aumg_9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvAumg9BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
